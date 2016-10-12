7f84176ef33371aaf25a9f4ec91ebf04cf6b9dc8
john <john@192.168.1.2>
2016-10-11 22:47:21 -0600

    Converted the index references to property fields for all column meta data

--- XYGraph/WebContent/js/XYGraph.js
+++ XYGraph/WebContent/js/XYGraph.js
@@ -7,14 +7,14 @@
  * The format object's properties can be functions that are applied to the column value.
  * 
  *  Each object has two properties
- *  	column name in the form of the actual property name
- *  	function to apply to that column value
+ *      column name in the form of the actual property name
+ *      function to apply to that column value
  * 
- * 		col['date'] = formatFunction
+ *         col['date'] = formatFunction
  * 
- * 		To apply this function
+ *         To apply this function
  * 
- * 		col['date'](args)
+ *    col['date'](args)
  * 
  *     Applying it to a property of a data object
  *     
@@ -35,14 +35,13 @@
  *     For iterating over the keys it would be
  *     
  *     for (prop in ft) {
- *     		//Taking the old value of d.value and formatting it then assigning it back to d.value.
- *     		d[prop] = ft[prop](d[prop]);
+ *             //Taking the old value of d.value and formatting it then assigning it back to d.value.
+ *             d[prop] = ft[prop](d[prop]);
  *     }
- * 		
+ *         
  * 
  * 
- *  To Do: 
- *  
+ * 
  *     1. Handle finding extent for dates over multiple data sets (arrays) FIXED
  *     2.  Handle if the data is provided by files or by a data store
  *     3. Set up axis title so that it can be centered and larger font
@@ -54,70 +53,63 @@
  * 
  */
 
-XYGraph = function(divPanelTag, metaData, dataSets, margin, gap) {
+XYGraph = function (divPanelTag, metaData, dataSets, margin, gap) {
 
-	var formatObject = metaData.formatObject;
-	var cols = metaData.cols;
-	
-    width = parseInt(d3.select(divPanelTag).style("width")) - margin*2,
-    height = parseInt(d3.select(divPanelTag).style("height")) - margin*2;
-	
-// cols['xType'] puts in the appropriate d3 class for scale like d3.scaleTime if the axis is dates
-var xScale = cols['xType']
-	.range([0, width]);
+    var formatObject = metaData.formatObject,
+        cols = metaData.cols,   
+        width = parseInt(d3.select(divPanelTag).style("width"), 10) - margin * 2,
+        height = parseInt(d3.select(divPanelTag).style("height"), 10) - margin * 2;
+    
+ // cols['xType'] puts in the appropriate d3 class for scale like d3.scaleTime if
+ // the axis is dates
+ var xScale = cols['xType']
+     .range([0, width]);
 
-// Use nice to round off the tick text to a nice round number
-//.nice(d3.time.year);
+ // Use nice to round off the tick text to a nice round number
+ // .nice(d3.time.year);
+ 
+ var yScale = cols['yType']
+     .range([height, 0]);
+ 
+ var line = d3.line()
+     .x(function (d) { return xScale(d[cols.x]); })
+     .y(function (d) { return yScale(d[cols.y]); });
+ 
+ var xGap = margin + gap;
+ var yGap = margin - gap;
+ 
+ var graph = d3.select(divPanelTag)
+      .attr("width", width)
+      .attr("height", height + margin * 2)
+      .append("g")
+      .attr("transform", "translate(" + xGap + "," + yGap + ")");
 
-var yScale = cols['yType']
-	.range([height, 0]);
 
-var line = d3.line()
-    .x(function(d) { return xScale(d[cols['x']]); })
-    .y(function(d) { return yScale(d[cols['y']]); });
-
-var xGap = margin + gap;
-var yGap = margin - gap;
-
-var graph = d3.select(divPanelTag)
-    .attr("width", width)
-    .attr("height", height + margin*2)
-   .append("g")
- .attr("transform", "translate(" + xGap + "," + yGap + ")");
-
-function formatData(data, formatObject) {
-	for (prop in formatObject) {
-		data[prop] = formatObject[prop](data[prop]);
-	}
-}
 
 
 function plotGraphs() {
-	
-		dataSets.forEach(function(dataSet) {
-			dataSet.forEach(function(d) {
-				formatData(d, formatObject);
-			});
-		});
+    
+    // Remove any old graph lines
+    d3.selectAll(".line").remove()
   
- 		xScale.domain([d3.min(dataSets, function(dataSet) {
-							return d3.min(dataSet, function(d) {
-								return d[cols['x']];
-							})
-						}),
-						d3.max(dataSets, function(dataSet) {
-							return d3.max(dataSet, function(d) {
-								return d[cols['x']];
-							})
-						})
-			]);
+         xScale.domain([d3.min(dataSets, function (dataSet) {
+                            return d3.min(dataSet, function (d) {
+                                return d[cols.x];
+                            })
+                        }),
+                        d3.max(dataSets, function (dataSet) {
+                            return d3.max(dataSet, function (d) {
+                                return d[cols.x];
+                            })
+                        })
+            ]);
  
   
-		  yScale.domain([0, d3.max(dataSets, function(dataSet) {
-			  return d3.max(dataSet, function(d) {
-				  return d[cols['y']];
-			  });
-		  } ) ] );
+          yScale.domain([0, d3.max(dataSets, function (dataSet) {
+              return d3.max(dataSet, function (d) {
+                  return d[cols.y];
+              });
+          } ) ] );
 
   //Offers 10 colors that can be accessed by c10(index)
   // in an iteration calling c10(i) will give you different colors for each step up to 10
@@ -133,11 +125,11 @@ function plotGraphs() {
       .call(xAxis)
      .append("text")
         .attr("id", "xAxisTitle")
-     	.attr("y",-16)
-     	.attr("x", width - 100)
-     	.attr("dy", ".71em")
-     	.style("text-anchor", "end")
-     	.text(cols["xAxisTitle"])
+         .attr("y",-16)
+         .attr("x", width - 100)
+         .attr("dy", ".71em")
+         .style("text-anchor", "end")
+         .text(cols.xAxisTitle)
 
   // Called once - same axis works for all data sets
   graph.append("g")
@@ -149,11 +141,11 @@ function plotGraphs() {
       .attr("y", 6)
       .attr("dy", ".71em")
       .style("text-anchor", "end")
-      .text(cols["yAxisTitle"]);
+      .text(cols.yAxisTitle);
 
   // Called as many times as there are data sets  
-  dataSets.forEach(function(dataSet,i) {
-	  graph.append("path")
+  dataSets.forEach(function (dataSet,i) {
+      graph.append("path")
       .datum(dataSet)
       .attr("class", "line")
       .style("stroke", c10(i))
@@ -165,15 +157,15 @@ function plotGraphs() {
    * Recalculates the graph to fit properly in whatever size the containing window is
    */
   function resize() {
-    var width = parseInt(d3.select(divPanelTag).style("width")) - margin*3,
-    height = parseInt(d3.select(divPanelTag).style("height")) - margin*2;
+    var width = parseInt(d3.select(divPanelTag).style("width")) - margin * 3,
+    height = parseInt(d3.select(divPanelTag).style("height")) - margin * 2;
 
     /* Update the range of the scale with new width/height */
     xScale.range([0, width]);
     yScale.range([height, 0]);
 
-    xAxis.ticks(Math.max(width/50, 2));
-    yAxis.ticks(Math.max(height/50, 2));
+    xAxis.ticks(Math.max(width / 50, 2));
+    yAxis.ticks(Math.max(height / 50, 2));
     
     /* Update the axis with the new scale */
     graph.select('.x.axis')
@@ -200,12 +192,23 @@ function plotGraphs() {
 
 
 return  {
-	plotGraph: function() {
-		plotGraphs();
-	}
-	
+    plotGraph: function () {
+        plotGraphs();
+        return this;
+    },
+    
+    setDataSets(newDataSets) {
+        dataSets = newDataSets;
+    },
+    
+    getDataSets() {
+        return dataSets;
+    }
+    
+    
+    
 }
-	
+    
 };
 
 
@@ -213,36 +216,52 @@ return  {
 var parseTime = d3.timeParse("%d-%b-%y");
 
 var formatObject = {
-		"date": parseTime, 
-		"close":Number
-		};
+    "date" : parseTime,
+    "close" : Number
+};
 
-var cols =  {	"x":"date", 
-					"y":"close",
-					"xType": d3.scaleTime()	,
-					"yType": d3.scaleLinear(),
-					"yAxisTitle": "Price ($)",
-					"xAxisTitle": "Date"
-					};
+var cols = {
+    "x" : "date",
+    "y" : "close",
+    "xType" : d3.scaleTime(),
+    "yType" : d3.scaleLinear(),
+    "yAxisTitle" : "Price ($)",
+    "xAxisTitle" : "Date"
+};
 
 
 
-var metaData = {};
-var dataSets = [];
+ var metaData = {},
+    dataSets = [],
+          xyGraph;
 
 metaData.cols = cols;
 metaData.formatObject = formatObject;
 
 d3.queue()
-	.defer(d3.csv, "data/data.csv")
-	.defer(d3.csv, "data/data2.csv")
-	.awaitAll(ready);
+    .defer(d3.csv, "data/data.csv")
+    .defer(d3.csv, "data/data2.csv")
+    .awaitAll(ready);
+
+function formatData(data, formatObject) {
+    for (prop in formatObject) {
+        data[prop] = formatObject[prop](data[prop]);
+    }
+}
+
 
 function ready(error, results) {
-	results.forEach(function(array) {
-		dataSets.push(array);
-	});
-	console.dir(dataSets);	
-	new XYGraph("#graph", metaData, dataSets, 40, 30).plotGraph();
+    results.forEach(function (array) {
+        dataSets.push(array);
+    });
+    console.dir(dataSets);    
+
+    dataSets.forEach(function (dataSet) {
+        dataSet.forEach(function (d) {
+            formatData(d, formatObject);
+        });
+    });
+    
+    xyGraph = new XYGraph("#graph", metaData, dataSets, 40, 30).plotGraph();
 }
 
